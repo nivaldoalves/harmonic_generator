138dcc30cc53cf8d18e384e1d75f37586eeb8fef
Nivaldo Alves <104028573+nivaldoalves@users.noreply.github.com>
2025-09-15 11:08:42 -0300

    changed excluir

--- harmonic-generator/src/app/confirmation-dialog/confirmation-dialog.component.ts
+++ harmonic-generator/src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -4,16 +4,15 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-confirmation-dialog',
   templateUrl: './confirmation-dialog.component.html',
-  styleUrls: ['./confirmation-dialog.component.scss']
+  styleUrls: ['./confirmation-dialog.component.scss'],
 })
 export class ConfirmationDialogComponent {
-
   constructor(
     public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { message: string }) { }
+    @Inject(MAT_DIALOG_DATA) public data: { message: string }
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
